54a355842340c2e3e70f9bb9a1a75e45678f4cd4
RonaldYou <ronaldrenyou@gmail.com>
2023-01-01 15:30:15 -0500

    now succesfully able to go to next page with enter key and onClick

--- indexScript.js
+++ indexScript.js
@@ -1,9 +1,19 @@
-document.getElementById("searchButton").addEventListener("click", getSearchVal);
-
 var searchVal;
 
+document.getElementById("searchBar").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        console.log("not okay");
+        getSearchVal.call();
+    }
+});
+document.getElementById("searchButton").addEventListener("click", getSearchVal);
+console.log(" okay");
+
 function getSearchVal(){
+    console.log("why am i not here");
     searchVal = document.getElementById("searchBar").value;
     sessionStorage.setItem("resortName", searchVal);
     window.location.href = "tow.html";
+    console.log(searchVal);
+    console.log("why am i not here!!!!!");
 }
