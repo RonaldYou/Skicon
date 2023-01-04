0433dd805858c579e47d5dfcff1da0e92c136ec4
RonaldYou <ronaldrenyou@gmail.com>
2023-01-04 18:37:57 -0500

    grabbing json objects and sorting values for lowest temp

--- towScript.js
+++ towScript.js
@@ -8,11 +8,49 @@ const options = {
 	}
 };
 let part1 = "https://ski-resort-forecast.p.rapidapi.com/";
-let part2 = "whistler";
-let part3 = "/forecast?units=m&el=bot";
+let part2 = resortName;
+let part3 = "/hourly?units=m&el=bot&c=true";
 
 let url = part1 + part2 + part3;
-fetch(url, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch(url, options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+fetch(url,options)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    displayBottom(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+function displayBottom(data){
+	const bottom = data.forecast;
+	var rawMinTemp = data.forecast[0].maxTemp;
+	var minTemp = rawMinTemp.slice(0,-2);
+	var minTempIndex = 0;
+	console.log(minTemp);
+	for(let i in bottom){
+		var rawTemp = data.forecast[i].maxTemp;
+		var temp = rawTemp.slice(0,-2);
+		if (parseInt(minTemp) > parseInt(temp)){
+			console.log(temp);
+			console.log(minTemp);
+			minTemp = temp;
+			minTempIndex = i;
+		}
+	}
+	// console.log("final");
+	// console.log(minTempIndex);
+	// console.log(minTemp);
+	console.log(data.forecast[minTempIndex].maxTemp);
+	// const bottomDiv = document.getElementById("bottom");
+	// const highOf =
+}
