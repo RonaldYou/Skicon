0a38c4df578e082a7b77daf043e38a3c82a0aaa2
RonaldYou <ronaldrenyou@gmail.com>
2023-01-03 21:25:18 -0500

    sucessfully read from api and string manipulate url

--- towScript.js
+++ towScript.js
@@ -1,1 +1,18 @@
 var resortName = sessionStorage.getItem("resortName");
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b3cf3ce7bemsh68f6de2ad6f5ffap108c6bjsn1ca8044ab34f',
+		'X-RapidAPI-Host': 'ski-resort-forecast.p.rapidapi.com'
+	}
+};
+let part1 = "https://ski-resort-forecast.p.rapidapi.com/";
+let part2 = "whistler";
+let part3 = "/forecast?units=m&el=bot";
+
+let url = part1 + part2 + part3;
+fetch(url, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
